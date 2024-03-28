8a83cccc7d5ad34c85b3b14ea56056bdac637f5d
DaviEspinosa <davipedrosa037@gmail.com>
2024-03-28 15:25:49 -0300

    ok

--- BD_&_FRONT/frontEnd/cadCliente.php
+++ BD_&_FRONT/frontEnd/cadCliente.php
@@ -7,24 +7,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obtém os valores do formulário
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $password = $_POST["password"];
     $tel = $_POST["number"];
 
     // Verifica se os campos não estão vazios
-    if (!empty($name) && !empty($email) && !empty($tel)) {
+    if (!empty($name) && !empty($email) && !empty($password) &&!empty($tel)) {
         
         // Query SQL com prepared statements
-        $query = "INSERT INTO Clientes (nome_cliente, email_cliente, telefone_cliente) VALUES ($1, $2, $3)";
+        $query = "INSERT INTO Clientes (nome_cliente, email_cliente, senha_cliente, telefone_cliente) VALUES ($1, $2, $3, $4)";
         
         // Prepara a consulta
         $resultado = pg_prepare($conectarBD, "", $query);
         
         if ($resultado) {
             // Executa a consulta
-            $resultado = pg_execute($conectarBD, "", array($name, $email, $tel));
+            $resultado = pg_execute($conectarBD, "", array($name, $email,$password, $tel));
             
             // Verifica se a consulta foi bem-sucedida
             if ($resultado) {
-                echo "Dados inseridos com sucesso no banco de dados.";
+                header("Location: login.html");
             } else {
                 echo "Erro ao inserir dados no banco de dados: " . pg_last_error($conectarBD);
             }
