41205e5f295ad4ee9c8af4c6423d6b1fa177089a
DaviEspinosa <davipedrosa037@gmail.com>
2024-03-28 17:01:04 -0300

    ok

--- BD_&_FRONT/frontEnd/loginCliente.php
+++ BD_&_FRONT/frontEnd/loginCliente.php
@@ -1,23 +1,10 @@
 <?php
-include ('conectar.php');
 
-// $password = $_POST['password'];
-$email = $_POST['email']; //senha_cliente,
+print_r($_REQUEST);
 
-$sql = "SELECT  email_cliente FROM Clientes WHERE email='$email'";//AND password='$password'
-$resultado = $conectarBD->query($sql);
-
-// if(!empty($password) && !empty($email)){
-
-// }
-
-// Verifica se a consulta retornou algum resultado
-if ($resultado->num_rows > 0) {
-    // Autenticação bem-sucedida
-    header("Location:index.html");
-    echo "Login bem-sucedido!";
-} else {
-    // Autenticação falhou
-    echo "Nome de usuário ou senha incorretos.";
+if (isset($_POST['submit'])) {
+    
 }
-
+else{
+    header("Location: login.html");
+}
