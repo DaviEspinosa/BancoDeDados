323084bb50595f9731e2c7cdf637945261b9f74b
DaviEspinosa <davipedrosa037@gmail.com>
2024-04-05 16:54:42 -0300

    ok

---
+++ ProjetoPizzariaBruno/index.php
@@ -0,0 +1,13 @@
+<?php
+include 'functions.php';
+
+?>
+
+<?=template_header("Pizzaria Dom BRuno")?>
+
+<div class="content">
+    <h2>Inicio</h2>
+    <p>Seja bem Vindo</p>
+</div>
+
+<?=template_footer()?>
